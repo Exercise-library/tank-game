ad499613466e831cb282bb80b942bf9f917f099e
robin-byte <robinbyte@163.com>
2020-11-03 01:07:03 +0800

    创建一个窗口，并设置其属性

--- src/main/java/com/tankGame/Start.java
+++ src/main/java/com/tankGame/Start.java
@@ -1,5 +1,9 @@
 package com.tankGame;
 
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 /**
  * 坦克大战游戏入口
  */
@@ -7,8 +11,18 @@ public class Start {
 
     public static void main(String[] args) {
 
+        JFrame jFrame = new JFrame();
+        jFrame.setSize(500,500);
+        jFrame.setVisible(true);
+        jFrame.setTitle("Tank game");
+        jFrame.setResizable(false);
 
-
+        jFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
 
     }
 
